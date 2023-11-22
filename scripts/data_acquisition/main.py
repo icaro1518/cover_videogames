741b357143a56c1a573e039dc93f5d1d5f96deac
Heiler Santiago Gómez Prieto <hsgomezp@unal.edu.co>
2023-11-21 22:04:50 -0500

    Actualización main adquisición datos

--- scripts/data_acquisition/main.py
+++ scripts/data_acquisition/main.py
@@ -7,3 +7,6 @@ results = client.get_all("he3q-86dn")
 
 # Convertir a DataFrame pandas
 results_df = pd.DataFrame.from_records(results)
+
+# Guardar como CSV
+results_df.to_csv("data/raw_data.csv", index=False)
